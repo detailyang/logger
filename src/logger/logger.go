3aefd47cbbeb7a515f34d321e3396ecaa515be38
detailyang <detailyang@gmail.com>
2015-10-10 12:32:04 +0800

    use bufio

---
+++ src/logger/logger.go
@@ -0,0 +1,14 @@
+/* 
+* @Author: detailyang
+* @Date:   2015-10-10 12:25:53
+* @Last Modified by:   detailyang
+* @Last Modified time: 2015-10-10 12:25:53
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+}
--- src/logger/main.go
+++ src/logger/main.go
@@ -2,40 +2,43 @@
 * @Author: detailyang
 * @Date:   2015-10-10 10:27:18
 * @Last Modified by:   detailyang
-* @Last Modified time: 2015-10-10 11:26:04
+* @Last Modified time: 2015-10-10 12:31:11
  */
 
 package main
 
 import (
-	//"bufio"
-    "log/syslog"
+	"bufio"
 	"log"
+	"log/syslog"
 	"os"
-    "syscall"
+	// "syscall"
 )
 
 func main() {
-    w, err := syslog.New(syslog.LOG_INFO, "ylogger")
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.SetOutput(w)
+	w, err := syslog.New(syslog.LOG_INFO, "ylogger")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(w)
 
-    log.Println("start up")
+	log.Println("start up")
 	f, err := os.OpenFile("/data/logs/tmp", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
 	if err != nil {
 		log.Println(err)
 	}
 	defer f.Close()
-    
 
-    buf := make([]byte, 65536)
+	reader := bufio.NewReader(os.Stdin)
 	for {
-        rb, err := syscall.Read(0, buf)
-        if err != nil {
-            log.Println(err)
-        }
-		f.Write(buf[:rb])
+		// rb, err := syscall.Read(0, buf)
+		line, isPrefix, err := reader.ReadLine()
+		//TODO isprefix
+		_ = isPrefix
+		if err != nil {
+			log.Println(err)
+		}
+		f.Write(line)
+		f.Write([]byte{'\n'})
 	}
 }
