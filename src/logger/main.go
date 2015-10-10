ac9e6154a1f3412bf42c6d726d457108137908bf
detailyang <detailyang@gmail.com>
2015-10-10 11:26:45 +0800

    add new line to file

--- src/logger/main.go
+++ src/logger/main.go
@@ -2,7 +2,7 @@
 * @Author: detailyang
 * @Date:   2015-10-10 10:27:18
 * @Last Modified by:   detailyang
-* @Last Modified time: 2015-10-10 10:34:56
+* @Last Modified time: 2015-10-10 11:26:04
  */
 
 package main
@@ -14,13 +14,22 @@ import (
 )
 
 func main() {
+	f, err := os.OpenFile("./tmp", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		line, err := reader.ReadString('\n')
+		line, isPrefix, err := reader.ReadLine()
+		//TODO isprefix
+		_ = isPrefix
 		if err != nil {
 			log.Println("get error ", err)
 			break
 		}
 		log.Println(line)
+		f.Write(line)
+		f.Write([]byte{'\n'})
 	}
 }
