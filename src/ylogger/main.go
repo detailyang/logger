5c30b59044549fae74ee9a2979a52f2d06d67386
detailyang <detailyang@gmail.com>
2015-10-11 21:24:52 +0800

    remove unsed log

--- src/ylogger/main.go
+++ src/ylogger/main.go
@@ -2,7 +2,7 @@
 * @Author: detailyang
 * @Date:   2015-10-10 13:13:06
 * @Last Modified by:   detailyang
-* @Last Modified time: 2015-10-11 21:24:17
+* @Last Modified time: 2015-10-11 21:24:42
  */
 
 package main
@@ -23,7 +23,6 @@ func main() {
 	flag.StringVar(&localFile, "localFile", "unix:///data/logs/logger/failover.log", "local file to log")
 	flag.Parse()
 
-	log.SetOutput(os.Stdout)
 	w, err := syslog.New(syslog.LOG_INFO, "ylogger")
 	if err != nil {
 		log.Println("[error] new syslog error ", err)
