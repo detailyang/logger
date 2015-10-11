2746354e215f36c9616451b3ceddf44f577eaf36
detailyang <detailyang@gmail.com>
2015-10-11 18:49:34 +0800

    fix typo error

--- src/logger/writter.go
+++ src/logger/writter.go
@@ -2,7 +2,7 @@
 * @Author: detailyang
 * @Date:   2015-10-10 13:36:16
 * @Last Modified by:   detailyang
-* @Last Modified time: 2015-10-11 18:48:10
+* @Last Modified time: 2015-10-11 18:49:30
  */
 
 package logger
@@ -65,7 +65,7 @@ func NewWritterList(urls []string) *WritterList {
 				select {
 				case <-ticker.C:
 					if conn.Alive == false {
-						log.Printf("[error] resource %s is dead \r\n", resource.Name)
+						log.Printf("[error] resource %s is dead \r\n", conn.Name)
 						conn.Connect()
 					}
 				case <-conn.Stop:
