51ad967e32f941df09f48cdca255d4853eb445b5
detailyang <detailyang@gmail.com>
2015-10-12 10:13:12 +0800

    add version

--- Makefile
+++ Makefile
@@ -2,8 +2,7 @@ GOPATH := ${GOPATH}:$(shell pwd)
 .PHONY: clean test
 
 all:
-		@GOPATH=$(GOPATH) go install ylogger
-
+		@GOPATH=$(GOPATH) go install -ldflags "-X main.Buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.Githash=`git rev-parse HEAD`" ylogger
 
 clean:
 		@rm -fr bin pkg
--- src/ylogger/main.go
+++ src/ylogger/main.go
@@ -2,25 +2,37 @@
 * @Author: detailyang
 * @Date:   2015-10-10 13:13:06
 * @Last Modified by:   detailyang
-* @Last Modified time: 2015-10-11 21:48:10
+* @Last Modified time: 2015-10-12 10:12:56
  */
 
 package main
 
 import (
 	"flag"
+	"log"
 	"logger"
 )
 
+var Buildstamp, Githash string
+
 func main() {
 	var config, topic, localServer, remoteServer, localFile, logFile string
+	var version bool
+
 	flag.StringVar(&config, "config", "", "config file")
 	flag.StringVar(&topic, "topic", "logger", "syslog tag (topic)")
 	flag.StringVar(&localServer, "localServer", "tcp://127.0.0.1:5140", "local server to log")
 	flag.StringVar(&remoteServer, "remoteServer", "tcp://127.0.1:5140", "remote server to log")
 	flag.StringVar(&localFile, "localFile", "unix:///data/logs/logger/failover.log", "local file to log")
 	flag.StringVar(&logFile, "logFile", "syslog", "default file (syslog) to log")
+	flag.BoolVar(&version, "version", false, "version")
 	flag.Parse()
+
+	if version == true {
+		log.Println("Git Commit Hash: ", Githash)
+		log.Println("UTC Build Time : ", Buildstamp)
+		return
+	}
 	l := logger.NewLogger(config, logFile, topic, localServer, remoteServer, localFile)
 	l.Run()
 }
